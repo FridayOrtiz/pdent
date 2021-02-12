8959b393f3ca2d0d5e29bc37c06fc4390de8c6d0
Rafael Ortiz <rafael@ortiz.sh>
2021-02-11 23:49:51 -0500

    Print the names of syscalls in the attached process as they're called.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod syscall_table;
+
+use nix::sys::ptrace as ptrace;
+use std::env;
+use nix::unistd::Pid;
+use nix::sys::wait::waitpid;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        std::process::exit(1);
+    }
+    let pid = Pid::from_raw((&args[1]).parse::<i32>().unwrap());
+    ptrace::attach(pid).unwrap();
+    waitpid(pid, None).unwrap();
+    loop {
+        ptrace::syscall(pid, None).unwrap();
+        waitpid(pid, None).unwrap();
+        let regs = ptrace::getregs(pid).unwrap();
+        println!("Syscall: {:?}", syscall_table::lookup(regs.orig_rax));
+    }
+    //ptrace::detach(pid, None).unwrap();
 }
